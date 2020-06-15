de13b059b5105ea1521d5cfb723f8106352033e9
Zhuchkou Artsiom <zidane.from.france@gmail.com>
2020-06-16 00:45:55 +0300

    RSL-01.1: add dependencys

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,13 +1,8 @@
-import { applyMiddleware, compose, createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import reducers from './reducers/index'
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
