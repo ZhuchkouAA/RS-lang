a1060e824ed31075ef74e78fdaf32cc72dbeb106
Zhuchkou Artsiom <zidane.from.france@gmail.com>
2020-06-16 14:51:03 +0300

    RSL-01.1: fix conflicts

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,8 +1,8 @@
-import { applyMiddleware, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import reducers from './reducers/index'
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index';
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
-export default store
+export default store;
