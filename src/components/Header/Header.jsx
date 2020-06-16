a5a3c7d501cc0e0ce92e4f2e827c25d6ba2f8b39
Daklin2 <daklin754@gmail.com>
2020-06-17 00:28:47 +0300

    RSL-3: presentation elements into components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import style from './Header.module.scss';
+import AuthClaster from './AuthClaster/AuthClaster';
+import NavbarToggler from './NavbarToggler/NavbarToggler';
 
 const Header = () => (
   <header className={style.header}>
     <div className={style.wrapper}>
-      <div className={style.navbartoggle}>
-        <div className={style.menutoggle}>
-          <span />
-        </div>
-      </div>
-      <div className={style.authcontainer}>
-        <span className={style.authbtn}>Sing In</span>
-        <span>/</span>
-        <span className={style.authbtn}>Sing Up</span>
-      </div>
+      <NavbarToggler />
+      <AuthClaster />
     </div>
   </header>
 );
