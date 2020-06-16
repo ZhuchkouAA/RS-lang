18865c3ac824b47f53ab5b36143b23f22afb6cbd
Daklin2 <daklin754@gmail.com>
2020-06-16 23:51:33 +0300

    RSL-3: create structer of header & create nav button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {
-  Route, BrowserRouter, Redirect, Switch,
-} from 'react-router-dom';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from '../../redux/redux-store';
 
 import PATH from '../../constants/path';
 
+import './App.module.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,17 @@
-import React from 'react'
+import React from 'react';
+import style from './Header.module.scss';
 
-const Header = () => <header>`header`</header>
+const Header = () => (
+  <header className={style.header}>
+    <div className={style.wrapper}>
+      <div className={style.navbartoggle}>
+        <div className={style.menutoggle}>
+          <span />
+        </div>
+      </div>
+      <div className="auth__container" />
+    </div>
+  </header>
+);
 
-export default Header
+export default Header;
