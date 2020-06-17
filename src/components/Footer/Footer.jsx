dfc4b45c1c75ac589ae09671ce0892ce65c2a404
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-17 11:04:04 +0300

    RSL-02: create footer and add two links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import styles from './Footer.module.scss';
 
 const Footer = () => {
-  return <footer>`footer`</footer>
-}
+  return (
+    <footer>
+      <div className={styles.footerwrapper}>
+        <div className={styles.footer__container}>
+          <a href="https://www.google.com" className={styles.footerlinkaboutUs}>
+            About us
+          </a>
+          <a href="https://www.google.com" className={styles.footerlinkAboutprogram}>
+            About the program
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
