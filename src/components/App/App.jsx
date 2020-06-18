7a29e6508957e8376ac1d8ea6aa306d32566b16c
Daklin2 <daklin754@gmail.com>
2020-06-18 06:09:50 +0300

    RSL-04: delete unnecessary brackets in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -26,33 +26,31 @@ import AboutUsPage from '../../pages/AboutUsPage';
 import ErrorPage from '../../pages/ErrorPage';
 
 const App = () => (
-  <>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={() => <Redirect to={PATH.SIGN_IN} />} />
-          <Route exact path={PATH.SIGN_IN} component={SignInPage} />
-          <Route exact path={PATH.MAIN} component={MainPage} />
-          <Route exact path={PATH.WORD_CARD} component={WordPage} />
-          <Route exact path={PATH.DICTIONARY} component={DictionaryPage} />
-          <Route exact path={PATH.SPEAK_IT} component={SpeakItPage} />
-          <Route exact path={PATH.ENGLISH_PUZZLE} component={EnglishPuzzlePage} />
-          <Route exact path={PATH.SAVANNA} component={SavannaPage} />
-          <Route exact path={PATH.AUDIO_CALL} component={AudioCallPage} />
-          <Route exact path={PATH.SPRINT} component={SprintPage} />
-          <Route exact path={PATH.OWN_GAME} component={OnwGamePage} />
-          <Route exact path={PATH.STATISTIC} component={StatisticPage} />
-          <Route exact path={PATH.SETTINGS} component={SettingsPage} />
-          <Route exact path={PATH.PROPMO} component={PromoPage} />
-          <Route exact path={PATH.ABOUT_US} component={AboutUsPage} />
-          <Route component={ErrorPage} />
-        </Switch>
-      </BrowserRouter>
-      <Footer />
-    </Provider>
-  </>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={() => <Redirect to={PATH.SIGN_IN} />} />
+        <Route exact path={PATH.SIGN_IN} component={SignInPage} />
+        <Route exact path={PATH.SIGN_UP} component={SignUpPage} />
+        <Route exact path={PATH.MAIN} component={MainPage} />
+        <Route exact path={PATH.DICTIONARY} component={DictionaryPage} />
+        <Route exact path={PATH.SPEAK_IT} component={SpeakItPage} />
+        <Route exact path={PATH.ENGLISH_PUZZLE} component={EnglishPuzzlePage} />
+        <Route exact path={PATH.SAVANNA} component={SavannaPage} />
+        <Route exact path={PATH.AUDIO_CALL} component={AudioCallPage} />
+        <Route exact path={PATH.SPRINT} component={SprintPage} />
+        <Route exact path={PATH.OWN_GAME} component={OnwGamePage} />
+        <Route exact path={PATH.STATISTIC} component={StatisticPage} />
+        <Route exact path={PATH.SETTINGS} component={SettingsPage} />
+        <Route exact path={PATH.PROPMO} component={PromoPage} />
+        <Route exact path={PATH.ABOUT_US} component={AboutUsPage} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </BrowserRouter>
+    <Footer />
+  </Provider>
 );
 
 export default App;
