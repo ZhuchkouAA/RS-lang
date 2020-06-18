0be389795d6c27500dd4417f9fe160c584d6ebbc
Daklin2 <daklin754@gmail.com>
2020-06-18 12:21:25 +0300

    RSL-03: fix names of styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,20 @@ import style from './Header.module.scss';
 
 const Header = () => (
   <header className={style.Header}>
-    <div className={style.Header__Wrapper}>
-      <div className={style.NavbarContainer}>
-        <div className={style.NavbarContainer__Button}>
-          <span />
+    <div className={style.Header__wrapper}>
+      <div className={style.Header__navbarContainer}>
+        <div className={style.Header__navbarContainer__button}>
+          <span className={style.Header__navbarContainer__button_dash} />
         </div>
       </div>
-      <div className={style.AuthContainer}>
-        <span className={style.AuthContainer__Button}>Sing In</span>
-        <span>/</span>
-        <span className={style.AuthContainer__Button}>Sing Up</span>
+      <div className={style.Header__authContainer}>
+        <button className={style.Header__authContainer__button} type="button">
+          Sing In
+        </button>
+        <span className="">/</span>
+        <button className={style.Header__authContainer__button} type="button">
+          Sing Up
+        </button>
       </div>
     </div>
   </header>
