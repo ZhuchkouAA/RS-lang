49f846ad76b2eec100f87a70327b06b3efdcfa20
Aleksandr Sharikov <notsaint.mail@gmail.com>
2020-06-19 21:22:41 +0300

    RSL-24: refactor again

--- src/helpers/common-utils.js
+++ src/helpers/common-utils.js
@@ -2,7 +2,7 @@ import PATH from '../constants/path';
 import SECTION from '../constants/section';
 
 const getGameInfo = () => {
-  const games = Object.values(SECTION).slice(1);
+  const games = SECTION.slice(1);
 
   return games.map(({ section, ...game }) => {
     return { path: PATH[section], ...game };
