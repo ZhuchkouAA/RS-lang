15cae3d3a36b73df7a465e8c1820648793525236
Roma <zohanblz@gmail.com>
2020-06-19 16:35:25 +0300

    feat: add progress bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
+<<<<<<< HEAD
 import AppWithRoutes from './AppWithRouter';
 import Authorization from './Authorization';
 import Header from '../Header';
@@ -13,6 +14,30 @@ const App = ({ token }) => {
 
   return (
     <>
+=======
+import ProgressBar from '../ProgressBar/ProgressBar';
+import SignInPage from '../../pages/SignInPage';
+import SignUpPage from '../../pages/SignUpPage';
+import MainPage from '../../pages/MainPage';
+import WordPage from '../../pages/WordPage';
+import DictionaryPage from '../../pages/DictionaryPage';
+import SpeakItPage from '../../pages/SpeakItPage';
+import EnglishPuzzlePage from '../../pages/EnglishPuzzlePage';
+import SavannaPage from '../../pages/SavannaPage';
+import AudioCallPage from '../../pages/AudioCallPage';
+import SprintPage from '../../pages/SprintPage';
+import OnwGamePage from '../../pages/OnwGamePage';
+import StatisticPage from '../../pages/StatisticPage';
+import SettingsPage from '../../pages/SettingsPage';
+import PromoPage from '../../pages/PromoPage';
+import AboutUsPage from '../../pages/AboutUsPage';
+import ErrorPage from '../../pages/ErrorPage';
+
+const App = () => (
+  <>
+    <Provider store={store}>
+      <ProgressBar max={1000} now={100} />
+>>>>>>> feat: add progress bar
       <Header />
       <NavBar />
       <BrowserRouter>{PageRoutes}</BrowserRouter>
