3258e3a5f988852e79f55aa3fb58875c463c9c02
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-19 20:07:20 +0300

    RSL-22: create page 'about us'

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-import PATH from '../../constants/path';
+import style from './AboutUsPage.module.scss';
 
-const AboutUsPage = () => (
-  <>
-    <p>AboutUsPage</p>
-    <NavLink to={PATH.SIGN_UP}>SignInPage</NavLink>
-  </>
-);
+import Student from '../../components/Student/index';
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <p>AboutUsPage</p>
+      <Student
+        year="27"
+        hobby="sport, swimming, dance"
+        contribution={['deld', 'restart', 'on', 'e over']}
+        photo="../../img/Araik.jpg"
+      />
+      <Student
+        year="20"
+        hobby="tututu"
+        contribution={['tatata', 'ff', 'on', 'gamr']}
+        photo="../../img/git.png"
+      />
+      <Student
+        year="20"
+        hobby="tututu"
+        contribution={['dted', 'rtart', 'n', 'gamf']}
+        photo="../../img/33.jpg"
+      />
+      <Student
+        year="20"
+        hobby="tututu"
+        contribution={['dted', 'rtart', 'n', 'gamf']}
+        photo="../../img/33.jpg"
+      />
+      <Student
+        year="20"
+        hobby="tututu"
+        contribution={['dted', 'rtart', 'n', 'gamf']}
+        photo="../../img/33.jpg"
+      />
+      <Student
+        year="20"
+        hobby="tututu"
+        contribution={['dted', 'rtart', 'n', 'gamf']}
+        photo="../../img/33.jpg"
+      />
+      <Student
+        year="20"
+        hobby="tututu"
+        contribution={['dted', 'rtart', 'n', 'gamf']}
+        photo="../../img/33.jpg"
+      />
+      <div className={style.DescriptionText}>
+        Description Description Description Description Description DescriptionDescription
+        Description Description Description Description DescriptionDescription Description
+        Description Description Description DescriptionDescription Description Description
+        Description Description DescriptionDescription Description Description Description
+        Description DescriptionDescription Description Description Description Description
+        DescriptionDescription Description Description Description Description
+        DescriptionDescription Description Description Description Description
+        DescriptionDescription Description Description Description Description Description
+      </div>
+    </div>
+  );
+};
 
 export default AboutUsPage;
