8166b295ac77cffac59efcc1ee28897e2898c847
Zhuchkou Artsiom <zidane.from.france@gmail.com>
2020-06-19 14:50:52 +0300

    RSL-08: refactor v0.6

--- src/middlewares/users/sign-up.js
+++ src/middlewares/users/sign-up.js
@@ -1,14 +1,13 @@
 import { isSignInRender, setAlertMessage } from '../../redux/actions/creators/sign-in-data';
 import postRequest from '../../helpers/fetch-utils/post-response';
-import URL from '../../constants/urls';
+import API_URLS from '../../constants/APIUrls';
 
 export default function signUp(login, password) {
   return (dispatch) => {
-    postRequest(URL.users, JSON.stringify({ email: login, password }))
+    postRequest(API_URLS.users.createUser, JSON.stringify({ email: login, password }))
       .then((response) => {
         if (!response.ok) {
           dispatch(setAlertMessage(response.statusText));
-          return response.json();
         }
         return response.json();
       })
