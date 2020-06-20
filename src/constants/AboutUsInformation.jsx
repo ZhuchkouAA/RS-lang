78827f8d0e332bf90a4c81f516d702ecf47c9c18
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-20 10:45:54 +0300

    RSL-22: deleted description students, use array.map

---
+++ src/constants/AboutUsInformation.jsx
@@ -0,0 +1,50 @@
+export default [
+  {
+    name: 'Victor',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+  {
+    name: 'Araik',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+  {
+    name: 'notSAINT',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+  {
+    name: 'Roman',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+  {
+    name: 'Ales',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+  {
+    name: 'daclin2',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+  {
+    name: 'Yana',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+  {
+    name: 'ZhuckouAA',
+    linkGit: 'https://github.com/Rasoan',
+    imgSrc:
+      'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+  },
+];
--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -4,52 +4,18 @@ import style from './AboutUsPage.module.scss';
 
 import Student from '../../components/Student/index';
 
+import studentsInformation from '../../constants/AboutUsInformation';
+
 const AboutUsPage = () => {
+  const studentsInformationZ = studentsInformation.map((game) => {
+    const { name, linkGit, imgSrc } = game;
+    return <Student name={name} linkGit={linkGit} imgSrc={imgSrc} key={334} />;
+  });
+
   return (
     <div>
       <p>AboutUsPage</p>
-      <Student
-        year="27"
-        hobby="sport, swimming, dance"
-        contribution={['deld', 'restart', 'on', 'e over']}
-        photo="../../img/Araik.jpg"
-      />
-      <Student
-        year="20"
-        hobby="tututu"
-        contribution={['tatata', 'ff', 'on', 'gamr']}
-        photo="../../img/git.png"
-      />
-      <Student
-        year="20"
-        hobby="tututu"
-        contribution={['dted', 'rtart', 'n', 'gamf']}
-        photo="../../img/33.jpg"
-      />
-      <Student
-        year="20"
-        hobby="tututu"
-        contribution={['dted', 'rtart', 'n', 'gamf']}
-        photo="../../img/33.jpg"
-      />
-      <Student
-        year="20"
-        hobby="tututu"
-        contribution={['dted', 'rtart', 'n', 'gamf']}
-        photo="../../img/33.jpg"
-      />
-      <Student
-        year="20"
-        hobby="tututu"
-        contribution={['dted', 'rtart', 'n', 'gamf']}
-        photo="../../img/33.jpg"
-      />
-      <Student
-        year="20"
-        hobby="tututu"
-        contribution={['dted', 'rtart', 'n', 'gamf']}
-        photo="../../img/33.jpg"
-      />
+      {studentsInformationZ}
       <div className={style.DescriptionText}>
         Description Description Description Description Description DescriptionDescription
         Description Description Description Description DescriptionDescription Description
