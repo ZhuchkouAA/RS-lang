21d8561eed40e760e4aeb56f3a45c5ee748afe2f
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-20 23:51:50 +0300

    RSL-02: add global colors

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
-import styles from './Footer.module.scss';
+import { Grid } from '@material-ui/core';
+import style from './Footer.module.scss';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className={styles['footer-wrapper']}>
-        <div className={styles['footer-container']}>
-          <a href="https://www.google.com" className={styles['footer-link-about-us']}>
-            About us
-          </a>
-          <a href="https://www.google.com" className={styles['footer-link-about-program']}>
-            About the program
-          </a>
-        </div>
-      </div>
+    <footer className={style.Footer}>
+      <Grid className={style.Footer__container} container direction="row" justify="flex-start">
+        <a className={style.Footer__linkRs} href="https://github.com/rolling-scopes-school">
+          <img
+            className={style.Footer__imgRs}
+            src="https://avatars3.githubusercontent.com/u/11501370?s=200&v=4"
+            width="50"
+            height="50"
+            alt="RS"
+          />
+          <p className={style.Footer__headerRs}>Rolling Scopes School</p>
+        </a>
+      </Grid>
     </footer>
   );
 };
