6373da1a2a19b127c0a1b2dc067923594495d838
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-20 10:45:54 +0300

    RSL-22: deleted description students, use array.map

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import Grid from '@material-ui/core/Grid';
+
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 
@@ -10,65 +12,40 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import style from './Student.module.scss';
 
-import studentPhotoSrc from '../../img/artworks.jpg';
-import gitimg from '../../img/git.png';
+import gitImg from '../../img/git.png';
 
-const Student = ({ year, hobby, contribution }) => {
-  const contribut = contribution.map((element) => (
-    <li className={style.ContributionStudent}>{element}</li>
-  ));
+const Student = ({ name, linkGit, imgSrc }) => {
   return (
     <section>
-      <div>
-        <div className={style.ContainerStudent}>
-          <Card className={style.CardStudent}>
-            <CardActionArea>
-              <CardMedia className={style.ImgStudent} image={studentPhotoSrc} title="photo" />
-              <Typography className={style.NameStudent} gutterBottom variant="h5" component="h2">
-                Name
-              </Typography>
-            </CardActionArea>
-            <Button className={style.LinkIconWrapper} size="small" color="primary">
-              <a href="signin">
-                <img width="50" height="50" src={gitimg} alt="git" />
-              </a>
-            </Button>
-            <Button className={style.LinkIconWrapper} size="small" color="primary">
-              <a href="signin">
-                <img width="50" height="50" src={gitimg} alt="git" />
-              </a>
-            </Button>
-            <Button className={style.LinkIconWrapper} size="small" color="primary">
-              <a href="signin">
-                <img width="50" height="50" src={gitimg} alt="git" />
-              </a>
-            </Button>
-          </Card>
-          <div className={style.StudentInformationContainer}>
-            <p className={style.StudentInformation}>
-              Year:
-              <span>{year}</span>
-            </p>
-            <p className={style.StudentInformation}>
-              Hobby:
-              <span>{hobby}</span>
-            </p>
-            <details className={style.StudentInformation}>
-              <summary className={style.StudentInformation__listHeader}>Contribution</summary>
-              <ul className={style.StudentInformation__list}>{contribut}</ul>
-            </details>
-          </div>
-        </div>
+      <Grid
+        className={style.CardStudent}
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Card className={style.CardStudent}>
+          <CardActionArea>
+            <CardMedia className={style.ImgStudent} image={imgSrc} title="photo" />
+            <Typography className={style.NameStudent} gutterBottom variant="h5" component="h2">
+              {name}
+            </Typography>
+          </CardActionArea>
+          <Button className={style.LinkIconWrapper} size="small" color="primary">
+            <a href={linkGit}>
+              <img width="100" height="100" src={gitImg} alt="git" />
+            </a>
+          </Button>
+        </Card>
         <div className={style.Strip} />
-      </div>
+      </Grid>
     </section>
   );
 };
 
 Student.propTypes = {
-  year: PropTypes.string.isRequired,
-  hobby: PropTypes.string.isRequired,
-  contribution: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  linkGit: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
 };
-
 export default Student;
