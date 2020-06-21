a7b528f8b558883cbe1dd6aeb4d20ed15cb5fb22
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-21 13:03:44 +0300

    RSL-22: fix key for component Student

--- src/constants/AboutUsInformation.jsx
+++ src/constants/AboutUsInformation.jsx
@@ -4,47 +4,55 @@ export default [
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'jRfMcPcj',
   },
   {
     name: 'Araik',
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'kRf6cEcn',
   },
   {
     name: 'notSAINT',
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'lYf5cBcl',
   },
   {
     name: 'Roman',
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'mZfKc5cv',
   },
   {
     name: 'Ales',
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'n5fqcYcz',
   },
   {
     name: 'daclin2',
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'o2fLcZcB',
   },
   {
     name: 'Yana',
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'pYfNcRcR',
   },
   {
     name: 'ZhuckouAA',
     linkGit: 'https://github.com/Rasoan',
     imgSrc:
       'https://avatars1.githubusercontent.com/u/60571068?s=460&u=b872461fe6f18ae929c32fc1cf0ca4a5b2e73d53&v=4',
+    id: 'qxfPcBcJ',
   },
 ];
