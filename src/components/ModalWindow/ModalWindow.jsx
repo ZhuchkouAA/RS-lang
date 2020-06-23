2b854225a337fcb5a5f77fd68c07e47b1ea25113
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-23 13:05:46 +0300

    RSL-27: create modal window

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import style from './ModalWindow.module.scss';
 
 const abc = () => {
-  return <div>hello</div>;
+  return <div className={style.experiment}>hello</div>;
 };
 
 export default abc;
