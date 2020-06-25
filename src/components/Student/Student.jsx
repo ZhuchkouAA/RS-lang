db4d57ca4537f811fb0d1d7c9727bff713c53c07
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-25 21:53:31 +0300

    RSL-22: corrected comments in pull request

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -1,55 +1,46 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
 
-import Grid from '@material-ui/core/Grid';
-
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import style from './Student.module.scss';
+import { Card, CardActionArea, CardMedia, Grid, Button, Typography } from '@material-ui/core/';
 
 import gitImg from '../../img/git.png';
 
+import style from './Student.module.scss';
+
 const Student = ({ name, linkGit, imgSrc }) => {
   return (
-    <section>
-      <Grid
-        className={style.Wrapper}
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <Card className={style.CardStudent}>
-          <CardActionArea>
-            <CardMedia className={style.CardStudent__imgStudent} image={imgSrc} title="photo" />
-            <Typography
-              className={style.CardStudent__nameStudent}
-              gutterBottom
-              variant="h5"
-              component="h2"
-            >
-              {name}
-            </Typography>
-          </CardActionArea>
-          <Button className={style.CardStudent__wrapperIconLink} size="small" color="primary">
-            <a className={style.CardStudent__link} href={linkGit}>
-              <img
-                className={style.CardStudent__img}
-                width="100"
-                height="100"
-                src={gitImg}
-                alt="git"
-              />
-            </a>
-          </Button>
-        </Card>
-      </Grid>
-    </section>
+    <Grid
+      className={style.CardStudent__wrapper}
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      <Card className={style.CardStudent}>
+        <CardActionArea>
+          <CardMedia className={style['CardStudent__img-student']} image={imgSrc} title="photo" />
+          <Typography
+            className={style['CardStudent__name-student']}
+            gutterBottom
+            variant="h5"
+            component="h2"
+          >
+            {name}
+          </Typography>
+        </CardActionArea>
+        <Button className={style['CardStudent__wrapper-icon-link']} size="small" color="primary">
+          <a className={style.CardStudent__link} href={linkGit}>
+            <img
+              className={style.CardStudent__img}
+              width="100"
+              height="100"
+              src={gitImg}
+              alt="git"
+            />
+          </a>
+        </Button>
+      </Card>
+    </Grid>
   );
 };
 
--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import style from './AboutUsPage.module.scss';
 
-import Student from '../../components/Student/index';
+import Student from '../../components/Student';
 
-import informationStudentArray from '../../constants/AboutUsInformation';
+import informationStudentArray from '../../constants/About-us-information.json';
 
 const AboutUsPage = () => {
   return (
     <Grid container direction="column" justify="center" alignItems="center">
-      {informationStudentArray.map(({ name, linkGit, imgSrc, id }) => (
-        <Student name={name} linkGit={linkGit} imgSrc={imgSrc} key={id} />
+      {informationStudentArray.map(({ name, linkGit, imgSrc }) => (
+        <Student
+          name={name}
+          linkGit={linkGit}
+          imgSrc={imgSrc}
+          key={`AboutUsPage_student-${name}`}
+        />
       ))}
       <div className={style.Description}>
         Description Description Description Description Description Description Description
