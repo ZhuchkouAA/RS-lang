29b22107f9f55d991a89b3b7eef09723f620b0c8
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-25 18:10:19 +0300

    RSL-34: fix footer in bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
           <img
             className={style.Footer__imgRs}
             src="https://avatars3.githubusercontent.com/u/11501370?s=200&v=4"
-            width="50"
-            height="50"
+            width="40"
+            height="40"
             alt="RS"
           />
           <p className={style.Footer__headerRs}>Rolling Scopes School</p>
