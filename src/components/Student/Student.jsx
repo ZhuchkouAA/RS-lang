9446b5ab5c3e272b0b2a884bfd0186a76b2cfb1d
Araik Rasoyan <araikrasoian@gmail.com>
2020-06-26 17:49:05 +0300

    RSL-22: reloaded the branch due to conflicts

---
+++ src/components/Student/Student.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Card, CardActionArea, CardMedia, Grid, Button, Typography } from '@material-ui/core/';
+
+import gitImg from '../../img/git.png';
+
+import style from './Student.module.scss';
+
+const Student = ({ name, linkGit, imgSrc }) => {
+  return (
+    <Grid
+      className={style.CardStudent__wrapper}
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      <Card className={style.CardStudent}>
+        <CardActionArea>
+          <CardMedia className={style['CardStudent__img-student']} image={imgSrc} title="photo" />
+          <Typography
+            className={style['CardStudent__name-student']}
+            gutterBottom
+            variant="h5"
+            component="h2"
+          >
+            {name}
+          </Typography>
+        </CardActionArea>
+        <Button className={style['CardStudent__wrapper-icon-link']} size="small" color="primary">
+          <a className={style.CardStudent__link} href={linkGit}>
+            <img
+              className={style['CardStudent__img-git']}
+              width="60"
+              height="60"
+              src={gitImg}
+              alt="git"
+            />
+          </a>
+        </Button>
+      </Card>
+    </Grid>
+  );
+};
+
+Student.propTypes = {
+  name: PropTypes.string.isRequired,
+  linkGit: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+};
+export default Student;
--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-import PATH from '../../constants/path';
+import Grid from '@material-ui/core/Grid';
+import style from './AboutUsPage.module.scss';
 
-const AboutUsPage = () => (
-  <>
-    <p>AboutUsPage</p>
-    <NavLink to={PATH.SIGN_UP}>SignInPage</NavLink>
-  </>
-);
+import Student from '../../components/Student';
+
+import informationStudentArray from '../../constants/About-us-information.json';
+
+const AboutUsPage = () => {
+  return (
+    <Grid container direction="column" justify="center" alignItems="center">
+      {informationStudentArray.map(({ name, linkGit, imgSrc }) => (
+        <Student
+          name={name}
+          linkGit={linkGit}
+          imgSrc={imgSrc}
+          key={`AboutUsPage_student-${name}`}
+        />
+      ))}
+      <div className={style.Description}>
+        Description Description Description Description Description Description Description
+        Description Description Description Description DescriptionDescription Description
+        Description Description Description DescriptionDescription Description Description
+        Description Description DescriptionDescription Description Description Description
+        Description DescriptionDescription Description Description Description Description
+        DescriptionDescription Description Description Description Description
+        DescriptionDescription Description Description Description Description
+        DescriptionDescription Description Description Description Description Description
+      </div>
+    </Grid>
+  );
+};
 
 export default AboutUsPage;
