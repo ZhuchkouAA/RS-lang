9203843d193310a6c2637419a7edc1d0f8409d4f
lordshtein <lordshtein@gmail.com>
2020-07-01 00:43:22 +0300

    RLS-34: add Savanna components

--- src/pages/SavannaPage/SavannaPage.jsx
+++ src/pages/SavannaPage/SavannaPage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect, useRef } from 'react';
 import { Grid } from '@material-ui/core/';
 import PropTypes from 'prop-types';
@@ -180,5 +181,47 @@ SavannaPage.propTypes = {
   words: PropTypes.arrayOf(PropTypes.object).isRequired,
   finallySendWordAndProgress: PropTypes.func.isRequired,
 };
+=======
+import React, { useState } from 'react';
+
+import SavannaQuestion from '../../components/SavannaQuestion';
+import SavannaAnswers from '../../components/SavannaAnswers';
+
+import style from './SavannaPage.module.scss';
+
+const SavannaPage = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const questions = [
+    {
+      id: 1,
+      question: 'Which statement about Hooks is not true?',
+    },
+    {
+      id: 2,
+      question: 'Which one is not a Hook?',
+    },
+    {
+      id: 3,
+      question: 'What Hook should be used for data fetching?',
+    },
+  ];
+
+  const nextQuestion = () =>
+    currentQuestion + 1 < questions.length ? currentQuestion + 1 : currentQuestion;
+
+  const timerId = setInterval(() => setCurrentQuestion(nextQuestion), 5000);
+  setTimeout(() => clearInterval(timerId), 20000);
+
+  const question = questions[currentQuestion];
+
+  return (
+    <div className={style.Savanna}>
+      <SavannaQuestion word={question.question} />
+      <SavannaAnswers answers={['123', 'asd', 'asd', 'qwe']} />
+    </div>
+  );
+};
+>>>>>>> RLS-34: add Savanna components
 
 export default SavannaPage;
