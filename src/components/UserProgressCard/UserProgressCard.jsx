b412cffee829afc41083d1340439c65aa982044b
Araik Rasoyan <araikrasoian@gmail.com>
2020-07-01 23:28:51 +0300

    RSL-25: add style for tooltipe

--- src/components/UserProgressCard/UserProgressCard.jsx
+++ src/components/UserProgressCard/UserProgressCard.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import styles from './UserProgressCard.module.scss';
-import ProgressBar from '../ProgressBar';
 
 import UserStateIndicator from '../UserStateindIcator';
 
 const UserProgressCard = () => {
   return (
     <Grid item className={styles.UserProgressCard}>
-      To do...RSL-25: Create User progress for main page
-      <ProgressBar />
-      <UserStateIndicator value={40} />
+      Процент правильных ответов сегодня:
+      <UserStateIndicator
+        value={7}
+        hint="Подсказка работает Подсказка работает Подсказка работает "
+      />
     </Grid>
   );
 };
--- src/components/UserStateindIcator/UserStateIndicator.jsx
+++ src/components/UserStateindIcator/UserStateIndicator.jsx
@@ -1,29 +1,54 @@
 // import React from 'react';
 // import PropTypes from 'prop-types';
-// import CircularProgress from '@material-ui/core/CircularProgress';
-// import Typography from '@material-ui/core/Typography';
-// import Box from '@material-ui/core/Box';
 
-// // import style from './UserStateIndicator.module.scss';
+// import { Box, Button, Tooltip, Typography, CircularProgress, makeStyles, withStyles } from '@material-ui/core';
+
+// import style from './UserStateIndicator.module.scss';
+
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     margin: theme.spacing(1),
+//   },
+//   customWidth: {
+//     maxWidth: 500,
+//   },
+//   noMaxWidth: {
+//     maxWidth: 'none',
+//   },
+//   abc: {
+//     backgroundColor: 'red',
+//   }
+// }));
 
 // const CircularProgressWithLabel = (props) => {
-//   const { value } = props;
-//   return (
-//     <Box position="relative" display="inline-flex">
-//       <CircularProgress variant="static" value={value} />
-//       <Box
-//         top={0}
-//         left={0}
-//         bottom={0}
-//         right={0}
-//         position="absolute"
-//         display="flex"
-//         alignItems="center"
-//         justifyContent="center"
-//       >
-//         <Typography variant="caption" component="div" color="textSecondary">{`${Math.round(value)}%`}</Typography>
+//   const { value, hint } = props;
+//   const classes = useStyles();
+
+//   const Circular = () => {
+//     return (
+//       <Box position="relative" display="inline-flex">
+//         <CircularProgress variant="static" value={value} />
+//         <Box
+//           top={0}
+//           left={0}
+//           bottom={0}
+//           right={0}
+//           position="absolute"
+//           display="flex"
+//           alignItems="center"
+//           justifyContent="center"
+//         >
+//           <Typography variant="caption" component="div" color="textSecondary">{`${Math.round(
+//             value
+//           )}%`}</Typography>
+//         </Box>
 //       </Box>
-//     </Box>
+//     );
+//   };
+
+
+//   return (
+//     <Tooltip title={hint} classes={{tooltip: classes.abc}}><Button className={classes.button}>{Circular()}</Button></Tooltip>
 //   );
 // };
 
