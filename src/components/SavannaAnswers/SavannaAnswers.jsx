6471be7d66c43fbfcb8cfe5472815a2903d3e57e
lordshtein <lordshtein@gmail.com>
2020-07-04 16:30:57 +0300

    RSL-34: add simpe game structure

--- src/components/SavannaAnswers/SavannaAnswers.jsx
+++ src/components/SavannaAnswers/SavannaAnswers.jsx
@@ -1,21 +1,34 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Grid } from '@material-ui/core';
 import Button from '../Button';
 
 import styles from './SavannaAnswers.module.scss';
 
-const SavannaAnswers = ({ answers }) => {
+const SavannaAnswers = ({ answers, handlerClick }) => {
   return (
     <div className={styles.answer__wrapper}>
-      {answers.map((answer) => {
-        return <Button text={answer} color="primary" />;
-      })}
+      <Grid container direction="row" justify="space-evenly" alignItems="center">
+        {answers.map((answer) => {
+          return (
+            <Button
+              text={answer}
+              color="primary"
+              handlerClick={() => handlerClick(answer)}
+              key={answer}
+            />
+          );
+        })}
+      </Grid>
     </div>
   );
 };
 
 SavannaAnswers.propTypes = {
-  answers: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.bool, PropTypes.string])).isRequired,
+  answers: PropTypes.arrayOf(PropTypes.string),
+  handlerClick: PropTypes.func.isRequired,
 };
 
 export default SavannaAnswers;
--- src/pages/SavannaPage/SavannaPage.jsx
+++ src/pages/SavannaPage/SavannaPage.jsx
@@ -1,42 +1,126 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Button from '../../components/Button';
 
 import SavannaQuestion from '../../components/SavannaQuestion';
 import SavannaAnswers from '../../components/SavannaAnswers';
 
 import style from './SavannaPage.module.scss';
 
+const answerArr = [];
+const gameSpeed = 2000;
+const questions = [
+  {
+    word: 'first',
+  },
+  {
+    word: 'second',
+  },
+  {
+    word: 'third',
+  },
+];
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+    return undefined;
+  }, [delay]);
+}
+
 const SavannaPage = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [lives, setLives] = useState(5);
+  const [showResult, setShowResult] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
 
-  const questions = [
-    {
-      id: 1,
-      question: 'Which statement about Hooks is not true?',
-    },
-    {
-      id: 2,
-      question: 'Which one is not a Hook?',
-    },
-    {
-      id: 3,
-      question: 'What Hook should be used for data fetching?',
-    },
-  ];
+  const endGame = () => {
+    if (currentQuestion + 1 === questions.length) {
+      setIsRunning(false);
+      setShowResult(true);
+    }
+    if (lives < 2) {
+      setIsRunning(false);
+      setShowResult(true);
+    }
+  };
 
-  const nextQuestion = () =>
-    currentQuestion + 1 < questions.length ? currentQuestion + 1 : currentQuestion;
+  const nextQuestion = () => {
+    if (currentQuestion + 1 < questions.length) {
+      return currentQuestion + 1;
+    }
+    return 5;
+  };
 
-  const timerId = setInterval(() => setCurrentQuestion(nextQuestion), 5000);
-  setTimeout(() => clearInterval(timerId), 20000);
+  useInterval(
+    () => {
+      if (answerArr.length === currentQuestion) {
+        answerArr.push('false');
+      }
+      setAnswers(answerArr);
+      endGame();
+      setCurrentQuestion(nextQuestion);
+    },
+    isRunning ? gameSpeed : null
+  );
+
+  const gameStart = () => {
+    setIsRunning(true);
+  };
 
   const question = questions[currentQuestion];
 
-  return (
-    <div className={style.Savanna}>
-      <SavannaQuestion word={question.question} />
-      <SavannaAnswers answers={['123', 'asd', 'asd', 'qwe']} />
-    </div>
-  );
+  const answerBtnClick = (answer) => {
+    if (answer !== question.word) {
+      setLives(lives - 1);
+    }
+    answerArr.push(answer);
+    setAnswers(answerArr);
+    endGame();
+  };
+
+  if (showResult) {
+    return (
+      <div>
+        {answers.map((el, index) => {
+          const key = index;
+          let res = 'верно';
+          if (el !== questions[key].word) {
+            res = 'ошибка';
+          }
+          return <span key={`${el}+${key}`}>{`${el} ${res}`}</span>;
+        })}
+      </div>
+    );
+  }
+  if (isRunning) {
+    return (
+      <div className={style.Savanna}>
+        <span>
+          {lives}
+          Жизни
+        </span>
+        <SavannaQuestion word={question.word} />
+        <SavannaAnswers
+          answers={['first', 'second', 'third', '44']}
+          handlerClick={answerBtnClick}
+        />
+      </div>
+    );
+  }
+  return <Button handlerClick={gameStart} text="Начать игру" color="secondary" />;
 };
 
 export default SavannaPage;
