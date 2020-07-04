0cdcb8b0177d64eaf1924ff7af8f9f97e838594f
lordshtein <lordshtein@gmail.com>
2020-07-04 16:30:57 +0300

    RSL-34: add simpe game structure

--- src/components/SavannaAnswers/SavannaAnswers.jsx
+++ src/components/SavannaAnswers/SavannaAnswers.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 import { Grid } from '@material-ui/core';
 import ButtonMU from '@material-ui/core/Button';
 
@@ -21,29 +20,13 @@ const SavannaAnswers = ({ answers, handlerClick }) => {
           );
         })}
       </Grid>
-=======
-import Button from '../Button';
-
-import styles from './SavannaAnswers.module.scss';
-
-const SavannaAnswers = ({ answers }) => {
-  return (
-    <div className={styles.answer__wrapper}>
-      {answers.map((answer) => {
-        return <Button text={answer} color="primary" />;
-      })}
->>>>>>> RLS-34: add Savanna components
     </div>
   );
 };
 
 SavannaAnswers.propTypes = {
-<<<<<<< HEAD
   answers: PropTypes.arrayOf(PropTypes.string).isRequired,
   handlerClick: PropTypes.func.isRequired,
-=======
-  answers: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.bool, PropTypes.string])).isRequired,
->>>>>>> RLS-34: add Savanna components
 };
 
 export default SavannaAnswers;
--- src/components/SavannaQuestion/SavannaQuestion.jsx
+++ src/components/SavannaQuestion/SavannaQuestion.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 import classNames from 'classnames';
 import styles from './SavannaQuestion.module.scss';
 
@@ -15,24 +14,13 @@ const SavannaQuestion = ({ word, animation }) => {
       >
         {word}
       </span>
-=======
-import styles from './SavannaQuestion.module.scss';
-
-const SavannaQuestion = ({ word }) => {
-  return (
-    <div className={styles.question}>
-      <span className={styles.question__wrapper}>{word}</span>
->>>>>>> RLS-34: add Savanna components
     </div>
   );
 };
 
 SavannaQuestion.propTypes = {
   word: PropTypes.string.isRequired,
-<<<<<<< HEAD
   animation: PropTypes.bool.isRequired,
-=======
->>>>>>> RLS-34: add Savanna components
 };
 
 export default SavannaQuestion;
