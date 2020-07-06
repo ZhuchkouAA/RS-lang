b2314a32b646802b321b44b660ff263541097fbb
Araik Rasoyan <araikrasoian@gmail.com>
2020-07-06 21:28:49 +0300

    RSL-22: added data and separated the mentor

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -3,15 +3,34 @@ import PropTypes from 'prop-types';
 
 import { Card, CardActionArea, CardMedia, Grid, Button, Typography } from '@material-ui/core/';
 
+import classNames from 'classnames';
 import gitImg from '../../img/git.png';
 
 import style from './Student.module.scss';
 
-const Student = ({ name, linkGit, imgSrc }) => {
+const Student = ({ name, linkGit, imgSrc, headerTextCard, type }) => {
+  const typeCard = classNames({
+    [style['CardStudent__wrapper-mentor']]: type.toLowerCase() === 'mentor',
+    [style['CardStudent__wrapper-developer']]: type.toLowerCase() === 'developer',
+  });
+  const typeHeaderCard = classNames({
+    [style['CardStudent__header-mentor']]: type.toLowerCase() === 'mentor',
+    [style['CardStudent__header-team-leader']]: type.toLowerCase() === 'team-leader',
+    [style['CardStudent__header-developer']]: type.toLowerCase() === 'developer',
+  });
   return (
-    <Grid className={style.CardStudent__wrapper}>
+    <Grid className={typeCard}>
       <Card className={style.CardStudent}>
         <CardActionArea>
+          <Typography
+            className={typeHeaderCard}
+            variant="h5"
+            component="h2"
+            gutterBottom
+            align="center"
+          >
+            {headerTextCard}
+          </Typography>
           <CardMedia className={style['CardStudent__img-student']} image={imgSrc} title="photo" />
           <Typography
             className={style['CardStudent__name-student']}
@@ -47,5 +66,7 @@ Student.propTypes = {
   name: PropTypes.string.isRequired,
   linkGit: PropTypes.string.isRequired,
   imgSrc: PropTypes.string.isRequired,
+  headerTextCard: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 export default Student;
--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -10,16 +10,18 @@ import informationStudentArray from '../../constants/About-us-information.json';
 const AboutUsPage = () => {
   return (
     <>
-      <Typography variant="h5" component="h2" gutterBottom align="center">
+      <Typography variant="h4" component="h2" gutterBottom align="center">
         Над приложением работали
       </Typography>
       <Grid className={style.Students__container} container justify="center">
-        {informationStudentArray.map(({ name, linkGit, imgSrc }) => (
+        {informationStudentArray.map(({ name, linkGit, imgSrc, headerTextCard, type }) => (
           <Student
             name={name}
             linkGit={linkGit}
             imgSrc={imgSrc}
             key={`AboutUsPage_student-${name}`}
+            headerTextCard={headerTextCard}
+            type={type}
           />
         ))}
       </Grid>
