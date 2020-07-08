ac1847db86fae116a9a0ca51782242f1a5e5613a
Roma <zohanblz@gmail.com>
2020-07-08 18:49:12 +0300

    RSL-SpeakIt: review

--- src/components/SpeakIt/speech.js
+++ src/components/SpeakIt/speech.js
@@ -7,17 +7,9 @@ const getSpeechRecognition = (successCallback) => {
   recognition.lang = 'en-US';
   recognition.interimResults = true;
 
-  // const recoStart = () => {
-  //   recognition.start();
-  // };
-
   recognition.onresult = (event) => {
     const result = event.results[event.results.length - 1][0].transcript;
     successCallback(result);
-  };
-
-  recognition.onend = () => {
-    recognition.start();
   };
 
   return recognition;
