edd32d032234c59232766f109983843122957700
bahnovak <zohanblz@gmail.com>
2020-07-10 22:07:07 +0300

    for test

--- src/components/SpeakIt/SpeakIt.jsx
+++ src/components/SpeakIt/SpeakIt.jsx
@@ -1,156 +1,178 @@
-import React, { useState } from 'react';
-import { Grid, Card, CardMedia, Button, Typography, Tab, Tabs } from '@material-ui/core';
-import {
-  Audiotrack,
-  LooksOne,
-  LooksTwo,
-  Looks3,
-  Looks4,
-  Looks5,
-  Looks6,
-  Mic,
-} from '@material-ui/icons';
-import classNames from 'classnames';
-
-import styles from './SpeakIt.module.scss';
-import getSpeechRecognition from './speech';
-import { getQueueMiniGame10 } from '../../helpers/games-utils/createQueueMiniGame';
-import Loader from '../Loader';
-
-const playAudio = (url) => {
-  const audio = new Audio();
-  audio.src = url;
-  audio.play();
-};
-
-const getWords = async (level = 1) => {
-  const path = 'https://raw.githubusercontent.com/zhuchkouaa/rslang-data/master/';
-  const res = await getQueueMiniGame10(level);
-  return res.map((element) => {
-    const obj = {};
-    obj.audio = path + element.optional.audio;
-    obj.image = path + element.optional.image;
-    obj.word = element.optional.word;
-    obj.wordTranslate = element.optional.wordTranslate;
-    obj.transcription = element.optional.transcription;
-    return obj;
-  });
-};
-
-const SpeakIt = () => {
-  const [value, setValue] = useState(0);
-  const [inputText, setInputText] = useState('');
-  const [wordsImage, setWordImage] = useState('');
-  const [isStartGame, setStartGame] = useState(false);
-  const [wordsExample, setWordExample] = useState(false);
-
-  const handleWordExample = (lvl) => {
-    getWords(lvl).then((val) => setWordExample(val));
-  };
-
-  handleWordExample();
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleInputText = (newValue) => {
-    setInputText(newValue);
-  };
-  const speechRec = getSpeechRecognition(handleInputText);
-  // const recoStart = () => {
-  //   speechRec.start();
-  // };
-  const handleStartGame = (start) => {
-    if (start && !isStartGame) {
-      speechRec.start();
-      setStartGame(start);
-    }
-
-    if (!start) {
-      speechRec.abort();
-      setStartGame(start);
-    }
-  };
-
-  const handleWordsImage = (url) => {
-    setWordImage(url);
-  };
-
-  return (
-    <Card className={styles.SpeakIt}>
-      {!wordsExample ? (
-        <Loader />
-      ) : (
-        <Grid item className={styles.SpeakIt__wrapper}>
-          <Tabs variant="scrollable" value={value} onChange={handleChange} aria-label="levels">
-            <Tab icon={<LooksOne />} aria-label="one" />
-            <Tab icon={<LooksTwo />} aria-label="two" />
-            <Tab icon={<Looks3 />} aria-label="three" />
-            <Tab icon={<Looks4 />} aria-label="four" />
-            <Tab icon={<Looks5 />} aria-label="five" />
-            <Tab icon={<Looks6 />} aria-label="six" />
-          </Tabs>
-          <CardMedia image={wordsImage} className={styles.SpeakIt__image} />
-          <Card className={styles.SpeakIt__word}>
-            {isStartGame && <Mic />}
-            <Typography>{inputText}</Typography>
-          </Card>
-          <Card className={styles.SpeakIt__containWords}>
-            {wordsExample.map((element) => {
-              return (
-                <Card
-                  className={classNames({
-                    [styles.SpeakIt__item]: true,
-                    [styles.SpeakIt__item_true]: isStartGame && element.word === inputText,
-                  })}
-                  key={Math.random()}
-                  onClick={(e) => {
-                    playAudio(element.audio, e);
-                    handleInputText(element.wordTranslate);
-                    handleWordsImage(element.image);
-                  }}
-                >
-                  <Audiotrack />
-                  <Typography className="word">{element.word}</Typography>
-                  <Typography>{element.transcription}</Typography>
-                </Card>
-              );
-            })}
-          </Card>
-          <Grid className={styles.SpeakIt__control}>
-            <Card className={styles.SpeakIt__button}>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => {
-                  handleStartGame(false);
-                  handleInputText('');
-                }}
-              >
-                Стоп
-              </Button>
-            </Card>
-            <Card className={styles.SpeakIt__button}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => {
-                  handleInputText('');
-                  handleStartGame(true);
-                }}
-              >
-                Старт
-              </Button>
-            </Card>
-            <Card className={styles.SpeakIt__button}>
-              <Button variant="contained">Статистика</Button>
-            </Card>
-          </Grid>
-        </Grid>
-      )}
-    </Card>
-  );
-};
-
-export default SpeakIt;
+// Pleaaase, uncomment me!!!! I want to job
+//
+//
+// import React, { useState, useEffect } from 'react';
+// import { Grid, Card, CardMedia, Button, Typography, Tab, Tabs } from '@material-ui/core';
+// import {
+//   Audiotrack,
+//   LooksOne,
+//   LooksTwo,
+//   Looks3,
+//   Looks4,
+//   Looks5,
+//   Looks6,
+//   Mic,
+// } from '@material-ui/icons';
+// import classNames from 'classnames';
+//
+// import styles from './SpeakIt.module.scss';
+// import getSpeechRecognition from './speech';
+// import { getQueueMiniGame10 } from '../../helpers/games-utils/createQueueMiniGame';
+// import Loader from '../Loader';
+//
+// const playAudio = (url) => {
+//   const audio = new Audio();
+//   audio.src = url;
+//   audio.play();
+// };
+//
+// const getWords = async (level = 1) => {
+//   const path = 'https://raw.githubusercontent.com/zhuchkouaa/rslang-data/master/';
+//   const res = await getQueueMiniGame10(level);
+//   return res.map((element) => {
+//     const obj = {};
+//     obj.audio = path + element.optional.audio;
+//     obj.image = path + element.optional.image;
+//     obj.word = element.optional.word;
+//     obj.wordTranslate = element.optional.wordTranslate;
+//     obj.transcription = element.optional.transcription;
+//     obj.defaultWord = element;
+//     obj.isGuested = false;
+//     return obj;
+//   });
+// };
+//
+// const SpeakIt = () => {
+//   const [value, setValue] = useState(0);
+//   const [inputText, setInputText] = useState('');
+//   const [wordsImage, setWordImage] = useState('');
+//   const [isStartGame, setStartGame] = useState(false);
+//   const [wordsExample, setWordExample] = useState(false);
+//
+//   const handleWordExample = (lvl) => {
+//     getWords(lvl).then((val) => setWordExample(val));
+//   };
+//   useEffect(() => {
+//     handleWordExample();
+//   }, []);
+//
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+//
+//   const handleInputText = (newValue) => {
+//     setInputText(newValue);
+//   };
+//   const speechRec = getSpeechRecognition(handleInputText);
+//   // const recoStart = () => {
+//   //   speechRec.start();
+//   // };
+//   const handleStartGame = (start) => {
+//     if (start && !isStartGame) {
+//       speechRec.start();
+//       setStartGame(start);
+//     }
+//
+//     if (!start) {
+//       speechRec.abort();
+//       setStartGame(start);
+//     }
+//   };
+//
+//   const handleWordsImage = (url) => {
+//     setWordImage(url);
+//   };
+//   // зацикл ререндер
+//   // useEffect(() => {
+//   //
+//   // }, [inputText]);
+//   //
+//   // const wordsCards = wordsExample.map((element) => {
+//   //   const isGuested = element.word === inputText || element.isGuested;
+//   //   if (element.word === inputText) {
+//   //     setWordExample(
+//   //       wordsExample.map((el) => {
+//   //         const word = el;
+//   //         if (element.word === el.word) {
+//   //           word.isGuested = true;
+//   //         }
+//   //         return word;
+//   //       })
+//   //     );
+//   //   }
+//   //   return (
+//   //     <Card
+//   //       className={classNames({
+//   //         [styles.SpeakIt__item]: true,
+//   //         [styles.SpeakIt__item_true]: isStartGame && isGuested,
+//   //       })}
+//   //       key={Math.random()}
+//   //       onClick={(e) => {
+//   //         playAudio(element.audio, e);
+//   //         handleInputText(element.wordTranslate);
+//   //         handleWordsImage(element.image);
+//   //       }}
+//   //     >
+//   //       <Audiotrack />
+//   //       <Typography className="word">{element.word}</Typography>
+//   //       <Typography>{element.transcription}</Typography>
+//   //     </Card>
+//   //   );
+//   // });
+//
+//   return (
+//     <Card className={styles.SpeakIt}>
+//       {!wordsExample ? (
+//         <Loader />
+//       ) : (
+//         <Grid item className={styles.SpeakIt__wrapper}>
+//           <Tabs variant="scrollable" value={value} onChange={handleChange} aria-label="levels">
+//             <Tab icon={<LooksOne />} aria-label="one" />
+//             <Tab icon={<LooksTwo />} aria-label="two" />
+//             <Tab icon={<Looks3 />} aria-label="three" />
+//             <Tab icon={<Looks4 />} aria-label="four" />
+//             <Tab icon={<Looks5 />} aria-label="five" />
+//             <Tab icon={<Looks6 />} aria-label="six" />
+//           </Tabs>
+//           <CardMedia image={wordsImage} className={styles.SpeakIt__image} />
+//           <Card className={styles.SpeakIt__word}>
+//             {isStartGame && <Mic />}
+//             <Typography>{inputText}</Typography>
+//           </Card>
+//           <Card className={styles.SpeakIt__containWords}>{wordsCards}</Card>
+//           <Grid className={styles.SpeakIt__control}>
+//             <Card className={styles.SpeakIt__button}>
+//               <Button
+//                 variant="contained"
+//                 color="secondary"
+//                 onClick={() => {
+//                   handleStartGame(false);
+//                   handleInputText('');
+//                 }}
+//               >
+//                 Стоп
+//               </Button>
+//             </Card>
+//             <Card className={styles.SpeakIt__button}>
+//               <Button
+//                 variant="contained"
+//                 color="primary"
+//                 onClick={() => {
+//                   handleInputText('');
+//                   handleStartGame(true);
+//                 }}
+//               >
+//                 Старт
+//               </Button>
+//             </Card>
+//             <Card className={styles.SpeakIt__button}>
+//               <Button variant="contained">Статистика</Button>
+//             </Card>
+//           </Grid>
+//         </Grid>
+//       )}
+//     </Card>
+//   );
+// };
+//
+// export default SpeakIt;
