888f4ac00d5ad5126af0a56543e8ae2bdf57e885
tritonJS826 <triton.68@mail.ru>
2020-07-15 23:55:18 +0300

    fix/RSL-72:add readme

--- src/components/SpeakIt/speech.js
+++ src/components/SpeakIt/speech.js
@@ -1,13 +1,13 @@
 const getSpeechRecognition = () => {
-  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-  const recognition = new SpeechRecognition();
-
-  recognition.continuous = false;
-  recognition.lang = 'en-US';
-  recognition.interimResults = true;
-
-  return recognition;
+  //   const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  //
+  //   const recognition = new SpeechRecognition();
+  //
+  //   recognition.continuous = false;
+  //   recognition.lang = 'en-US';
+  //   recognition.interimResults = true;
+  //
+  //   return recognition;
 };
 
 export default getSpeechRecognition;
